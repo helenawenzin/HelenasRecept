e857cb8b96a0433d2b4a7328b9485be8556c1f70
helenawenzin <helma254@hotmail.com>
2017-06-21 21:49:12 +0200

    first commit

---
+++ src/main/java/se/wenzin/HelenasRecept/HelenasReceptApplication.java
@@ -0,0 +1,14 @@
+package se.wenzin.HelenasRecept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelenasReceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelenasReceptApplication.class, args);
+
+	}
+
+}
